740af9d641f3848edf42a7ba917e13a470d9d45c
Dimagious <dmitrii.iurkin@yandex.ru>
2018-02-05 14:14:33 +0300

    Реинкарнация

--- src/main/java/app/MySpringBootApplication.java
+++ src/main/java/app/MySpringBootApplication.java
@@ -1,24 +1,26 @@
-package app;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-
-/**
- * Created by Dmitriy Yurkin on 31.01.2018.
- */
-
-@SpringBootApplication
-@ComponentScan(basePackages = {"app", "controllers", "services", "db.dao"})
-public class MySpringBootApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(MySpringBootApplication.class); /*говорим где искать наше приложение*/
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(MySpringBootApplication.class, args);
-    }
-}
+//package app;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//import org.springframework.context.annotation.ComponentScan;
+//
+///**
+// * Created by Dmitriy Yurkin on 31.01.2018.
+// */
+//@SpringBootApplication
+//@ComponentScan(basePackages = {"app", "controllers", "services", "db.dao"})
+//public class MySpringBootApplication
+//        extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(MySpringBootApplication.class); /*говорим где искать наше приложение*/
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(MySpringBootApplication.class, args);
+//    }
+//}
